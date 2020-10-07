a765789aa763fd9b1b69f5aca95b11bf51ea2b95
Pranav Tetali <ptetali0801@gmail.com>
2020-10-07 04:09:09 -0500

    Homework 1 1.12

---
+++ Homework2-6.17-CIS2348.py
@@ -0,0 +1,24 @@
+#Pranav Tetali
+#ID 1541822
+#Homework 2 6.17
+
+print("")
+simplepassword=input()
+password=''
+for x in simplepassword:
+    if(x=='i'):
+        password+="!"          # can also be written as: password=password+"!"
+    elif(x=='a'):
+        password+="@"          # replaces a with @
+    elif(x=='m'):
+        password+="M"          # replaces m with M
+    elif(x=='B'):
+        password+="8"          # replaces B with 8
+    elif(x=='o'):
+        password+="."          # replaces o with .
+    else:
+        password+= x         # if the password does not have i,a,m,B,and o it returns to the original
+
+password+="q*s"              # it adds q*s at the end
+print("Stronger Password:")
+print(password)
