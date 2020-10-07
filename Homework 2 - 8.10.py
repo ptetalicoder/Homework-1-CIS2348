0ff20a253ae4ff9cc4e1e3433ab446f3e89b91db
Pranav Tetali <ptetali0801@gmail.com>
2020-10-07 03:54:42 -0500

    Homework 1 1.12

---
+++ Homework 2 - 8.10.py
@@ -0,0 +1,23 @@
+#Pranav Tetali
+#ID 1541822
+#Homework 2 8.10
+
+s = input("")
+low = 0
+high = len(s)-1
+result = True
+while(low<high):
+    if(s[low]==' '):
+        low+=1
+    elif(s[high]==' '):
+        high-=1
+    elif(s[low]!=s[high]):
+        result = False
+        break
+    else:
+        low+=1
+        high-=1
+if (result):
+    print(s,"is a palindrome")
+else:
+    print(s,"is not a palindrome")
---
+++ Homework 2- 9.10.py
@@ -0,0 +1,24 @@
+#Pranav Tetali
+#ID 1541822
+#Homework 2 9.10
+
+import csv
+fileName = input("")
+
+
+wordsFrequency = {}
+
+with open(fileName, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+
+    for row in csvreader:
+        for word in row:
+
+            if word not in wordsFrequency.keys():
+                wordsFrequency[word] = 1
+
+            else:
+                wordsFrequency[word] += 1    #increase by 1
+
+for key in wordsFrequency.keys():
+    print(key + " " + str(wordsFrequency[key]))
